852f7260b574656f8178c6f6c135d37da006545a
Tim Seufert <116903809+Timhongphuc@users.noreply.github.com>
2026-06-10 22:42:55 +0200

    Minor design updates

--- src/App.tsx
+++ src/App.tsx
@@ -34,13 +34,13 @@ export function App() {
       <div>
         <h1 className="jdName">Stephan Seufert</h1>
 
-        <h3 className="briefDescription">Senior Software Engineer at <a className="databricks" href="https://www.databricks.com">Databricks</a> </h3>
+        <h3 className="briefDescription">Senior Software Engineer at <a className="databricks" href="https://www.databricks.com" target="_blank">Databricks</a> </h3>
       </div>
 
       <div className="introTextP space-y-4">
             <p>I am a Berlin-based Senior Software Engineer at Databricks with a primary interest in database systems and data infrastructure.</p>
-            <p>My background combines advanced research—holding a <a className="pLanguagesN" href="https://web.archive.org/web/20200301122807id_/https://publikationen.sulb.uni-saarland.de/bitstream/20.500.11880/25476/1/thesis_final.pdf">Ph.D. in Computer Science</a>—with extensive industry experience building large-scale distributed systems.</p>
-            <p>Over the years, I have optimized query compilers for Amazon Redshift, developed worldwide customer-facing retail search features, and engineered scalable data infrastructure utilizing <a className="pLanguages" href="https://www.java.com/">Java</a>, <a className="pLanguages" href="https://kafka.apache.org/">Apache Kafka</a>, and <a className="pLanguages" href="https://kubernetes.io/">Kubernetes</a>.</p>
+            <p>My background combines advanced research—holding a <a className="pLanguagesN" href="https://web.archive.org/web/20200301122807id_/https://publikationen.sulb.uni-saarland.de/bitstream/20.500.11880/25476/1/thesis_final.pdf" target="_blank">Ph.D. in Computer Science</a>—with extensive industry experience building large-scale distributed systems.</p>
+            <p>Over the years, I have optimized query compilers for Amazon Redshift, developed worldwide customer-facing retail search features, and engineered scalable data infrastructure utilizing <a className="pLanguages" href="https://www.java.com/" target="_blank">Java</a>, <a className="pLanguages" href="https://kafka.apache.org/" target="_blank">Apache Kafka</a>, and <a className="pLanguages" href="https://kubernetes.io/" target="_blank">Kubernetes</a>.</p>
             <p>I thrive on turning complex architectural challenges into highly scalable, efficient, and reliable engineering solutions.</p>
       </div>
 
@@ -51,8 +51,8 @@ export function App() {
       </div>
 
       <div className="introTextP space-y-4">
-            <p> <a className="awsdeequ" href="https://github.com/awslabs/deequ">AWS Deequ</a> • Deequ is a library built on top of Apache Spark for defining "unit tests for data", which measure data quality in large datasets. </p>
-            <p> <a className="ferrari" href="https://github.com/steps/Ferrari">Ferrari</a> • The Ferrari Reachability Index is a main-memory index structure for rapid processing of reachability queries over massive graphs. </p>
+            <p> <a className="awsdeequ" href="https://github.com/awslabs/deequ" target="_blank">AWS Deequ</a> • Deequ is a library built on top of Apache Spark for defining "unit tests for data", which measure data quality in large datasets. </p>
+            <p> <a className="ferrari" href="https://github.com/steps/Ferrari" target="_blank">Ferrari</a> • The Ferrari Reachability Index is a main-memory index structure for rapid processing of reachability queries over massive graphs. </p>
             <p></p>
             <p></p>
       </div>  
@@ -70,7 +70,7 @@ export function App() {
       <div>
         <hr />
       
-        <footer className="footerLine">(c) 2026 Crafted by Tim Seufert</footer>
+        <footer className="footerLine">(c) 2026 Crafted by <a className="TSUnderline" href="https://www.timhongphuc.de" target="_blank">Tim Seufert</a> </footer>
       </div>
       
     </div>
